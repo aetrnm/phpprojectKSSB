76f6b66c499bf2042bea71da345ea3f2fcd20a45
Oleg Gnat <oleggnatkovskii@gmail.com>
2023-02-01 22:37:35 +0100

    refactor: rename index.php to router.php

---
+++ router.php
@@ -0,0 +1,32 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+  case '':
+  case '/' :
+    require __DIR__ . '/index.php';
+    break;
+  case '/about' :
+    require __DIR__ . '/about.html';
+    break;
+  case '/shop' :
+    require __DIR__ . '/shop.html';
+    break;
+  case '/login' :
+    require __DIR__ . '/login.html';
+    break;
+  case '/register' :
+    require __DIR__ . '/register.html';
+    break;
+  case '/profile' :
+    require __DIR__ . '/profile.html';
+    break;
+  case '/add-book' :
+    require __DIR__ . '/add-book.html';
+    break;
+  default:
+    http_response_code(404);
+    require __DIR__ . '/404.html';
+    break;
+}
