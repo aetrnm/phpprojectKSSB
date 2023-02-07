4c7106a6f53abf1a57f3fbe008ca945cd1dc42e5
Oleg Gnat <oleggnatkovskii@gmail.com>
2023-02-07 22:41:59 +0100

    feat: read POST data

--- add-book.php
+++ add-book.php
@@ -1,1 +1,16 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] != 'POST' /*&& isset($_POST['submit'])*/) {
+  return;
+}
+
+$connect = mysqli_connect('localhost', 'root', '', 'bookstore') or die('Connection Failure' . mysqli_connect_error());
+if(!isset($_POST['title']) || !(isset($_POST['ISBN10']) || isset($_POST['ISBN13'])) || !isset($_POST['price'])) {
+  return;
+}
+
+$title = $_POST['title'];
+$author = $_POST['author'];
+$coverLink = $_POST['coverLink'];
+$ISBN10 = $_POST['ISBN10'];
+$ISBN13 = $_POST['ISBN13'];
+$price = $_POST['price'];
