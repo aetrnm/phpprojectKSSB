ee538c941ce5303689c0f3bfe866662ab1b0d839
Oleg Gnat <oleggnatkovskii@gmail.com>
2023-02-12 01:32:47 +0100

    refactor: adjust file names

--- router.php
+++ router.php
@@ -8,25 +8,28 @@ switch ($request) {
     require __DIR__ . '/index.php';
     break;
   case '/about' :
-    require __DIR__ . '/about.html';
+    require __DIR__ . '/about.php';
     break;
   case '/shop' :
-    require __DIR__ . '/shop.html';
+    require __DIR__ . '/shop.php';
     break;
   case '/login' :
-    require __DIR__ . '/login.html';
+    require __DIR__ . '/login.php';
     break;
   case '/register' :
-    require __DIR__ . '/register.html';
+    require __DIR__ . '/register.php';
     break;
   case '/profile' :
     require __DIR__ . '/profile.php';
     break;
   case '/add-book' :
-    require __DIR__ . '/add-book.html';
+    require __DIR__ . '/add-book.php';
+    break;
+  case '/guestbook' :
+    require __DIR__ . '/guestbook.php';
     break;
   default:
     http_response_code(404);
-    require __DIR__ . '/404.html';
+    require __DIR__ . '/404.php';
     break;
 }
