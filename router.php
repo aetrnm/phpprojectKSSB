8c7f1b76f052e919af508f8b3d4daeb30d0089aa
Oleg Gnat <oleggnatkovskii@gmail.com>
2023-02-26 12:44:42 +0100

    feat: add cart and checkout routers

--- router.php
+++ router.php
@@ -3,33 +3,39 @@
 $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
-  case '':
-  case '/' :
-    require __DIR__ . '/index.php';
-    break;
-  case '/about' :
-    require __DIR__ . '/about.php';
-    break;
-  case '/shop' :
-    require __DIR__ . '/shop.php';
-    break;
-  case '/login' :
-    require __DIR__ . '/login.php';
-    break;
-  case '/register' :
-    require __DIR__ . '/register.php';
-    break;
-  case '/profile' :
-    require __DIR__ . '/profile.php';
-    break;
-  case '/add-book' :
-    require __DIR__ . '/add-book.php';
-    break;
-  case '/guestbook' :
-    require __DIR__ . '/guestbook.php';
-    break;
-  default:
-    http_response_code(404);
-    require __DIR__ . '/404.php';
-    break;
+    case '':
+    case '/' :
+        require __DIR__ . '/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/about.php';
+        break;
+    case '/shop' :
+        require __DIR__ . '/shop.php';
+        break;
+    case '/login' :
+        require __DIR__ . '/login.php';
+        break;
+    case '/register' :
+        require __DIR__ . '/register.php';
+        break;
+    case '/profile' :
+        require __DIR__ . '/profile.php';
+        break;
+    case '/add-book' :
+        require __DIR__ . '/add-book.php';
+        break;
+    case '/guestbook' :
+        require __DIR__ . '/guestbook.php';
+        break;
+    case '/cart' :
+        require __DIR__ . '/cart.php';
+        break;
+    case '/checkout' :
+        require __DIR__ . '/checkout.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/404.php';
+        break;
 }
